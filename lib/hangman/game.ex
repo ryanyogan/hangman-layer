25c45390849b7278c597ade30efcd06b8a0c42cc
Ryan Yogan <ryanyogan@gmail.com>
2020-12-26 08:02:11 -0600

    validate make move, letter used

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -1,11 +1,33 @@
 defmodule Hangman.Game do
   defstruct turns_left: 7,
             game_state: :initializing,
-            letters: []
+            letters: [],
+            used: MapSet.new()
 
   def new_game() do
     %__MODULE__{
       letters: Dictionary.random_word() |> String.codepoints()
     }
+  end
+
+  def make_move(%{game_state: state} = game, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+
+  def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    {game, tally(game)}
+  end
+
+  defp accept_move(game, _guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  defp accept_move(game, guess, _already_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+  end
+
+  defp tally(_game) do
+    123
   end
 end
